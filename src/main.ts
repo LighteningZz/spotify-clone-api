1c0c38ad659a3c7f81bfe957eeae21b5f18c4661
Richard <richard.realtech@gmail.com>
2024-09-08 20:14:43 +0700

    use Global ValidatePipe

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,7 +17,7 @@ async function bootstrap() {
     jsonDocumentUrl: 'swagger.json',
     useGlobalPrefix: false,
   });
-
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
 bootstrap();
