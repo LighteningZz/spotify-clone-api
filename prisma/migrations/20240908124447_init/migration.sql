5f0ed4c03a54d300a06b9cff0525bb4b0b98477b
Richard <richard.realtech@gmail.com>
2024-09-08 19:49:06 +0700

    generate migration

--- prisma/migrations/20240908124447_init/migration.sql
+++ prisma/migrations/20240908124447_init/migration.sql
@@ -2,17 +2,17 @@
 CREATE TYPE "Gender" AS ENUM ('Male', 'Female', 'NonBinary', 'Other', 'NotSpecified');
 
 -- CreateTable
-CREATE TABLE "User" (
+CREATE TABLE "Users" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "name" TEXT,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
-CREATE TABLE "Profile" (
+CREATE TABLE "Profiles" (
     "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "bio" TEXT NOT NULL,
     "image" TEXT NOT NULL,
@@ -20,7 +20,7 @@ CREATE TABLE "Profile" (
     "gender" "Gender" NOT NULL,
     "userId" UUID NOT NULL,
 
-    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "Profiles_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -50,38 +50,14 @@ CREATE TABLE "Tracks" (
     CONSTRAINT "Tracks_pkey" PRIMARY KEY ("id")
 );
 
--- CreateTable
-CREATE TABLE "Podcasts" (
-    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "host" TEXT NOT NULL,
-
-    CONSTRAINT "Podcasts_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "PodcastEpisode" (
-    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
-    "title" TEXT NOT NULL,
-    "description" TEXT NOT NULL,
-    "releaseDate" TIMESTAMP(3) NOT NULL,
-    "host" TEXT NOT NULL,
-
-    CONSTRAINT "PodcastEpisode_pkey" PRIMARY KEY ("id")
-);
+-- CreateIndex
+CREATE UNIQUE INDEX "Users_email_key" ON "Users"("email");
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
-
--- CreateIndex
-CREATE UNIQUE INDEX "PodcastEpisode_host_key" ON "PodcastEpisode"("host");
+CREATE UNIQUE INDEX "Profiles_userId_key" ON "Profiles"("userId");
 
 -- AddForeignKey
-ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Profiles" ADD CONSTRAINT "Profiles_userId_fkey" FOREIGN KEY ("userId") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Albums" ADD CONSTRAINT "Albums_artistsId_fkey" FOREIGN KEY ("artistsId") REFERENCES "Artists"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
